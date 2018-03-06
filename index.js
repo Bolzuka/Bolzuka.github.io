d7d3ad3bd2c8689a2f0fff509766003e3a75e394
Shibaeva Maria <Bolzuka@gmail.com>
2018-03-06 15:40:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let moment = require("moment");
+let faker = require("faker");
+let pre = document.createElement('pre');
+pre.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a') + '<br>' + faker.internet.email();
+document.body.appendChild(pre);
